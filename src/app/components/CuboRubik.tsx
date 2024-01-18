b737d9c2aa07386f7a730ad8e75aad80edb4cb5f
WilsonSanFullStack <clinicasystemlab@gmail.com>
2024-01-18 03:39:00 -0500

    component cubo

---
+++ src/app/components/CuboRubik.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const CuboRubik = () => {
+  return (
+    <div className="w-2/5 m-4  justify-center items-center">
+      <div className="containerCara">
+        {/*//todo section color green */}
+        <section className="cara">
+          <div className="green">1</div>
+          <div className="green">2</div>
+          <div className="green">3</div>
+          <div className="green">4</div>
+          <div className="green">5</div>
+          <div className="green">6</div>
+          <div className="green">7</div>
+          <div className="green">8</div>
+          <div className="green">9</div>
+        </section>
+      </div>
+
+      <div className="containerCara3">
+        {/*//todo section color red */}
+        <section className="cara">
+          <div className="red">1</div>
+          <div className="red">2</div>
+          <div className="red">3</div>
+          <div className="red">4</div>
+          <div className="red">5</div>
+          <div className="red">6</div>
+          <div className="red">7</div>
+          <div className="red">8</div>
+          <div className="red">9</div>
+        </section>
+        {/*//todo section color white */}
+        <section className="cara">
+          <div className="white">1</div>
+          <div className="white">2</div>
+          <div className="white">3</div>
+          <div className="white">4</div>
+          <div className="white">5</div>
+          <div className="white">6</div>
+          <div className="white">7</div>
+          <div className="white">8</div>
+          <div className="white">9</div>
+        </section>
+        {/*//todo section color orange */}
+        <section className="cara">
+          <div className="orange">1</div>
+          <div className="orange">2</div>
+          <div className="orange">3</div>
+          <div className="orange">4</div>
+          <div className="orange">5</div>
+          <div className="orange">6</div>
+          <div className="orange">7</div>
+          <div className="orange">8</div>
+          <div className="orange">9</div>
+        </section>
+      </div>
+
+      <div className="containerCara">
+        {/*//todo section color blue */}
+        <section className="cara">
+          <div className="blue">1</div>
+          <div className="blue">2</div>
+          <div className="blue">3</div>
+          <div className="blue">4</div>
+          <div className="blue">5</div>
+          <div className="blue">6</div>
+          <div className="blue">7</div>
+          <div className="blue">8</div>
+          <div className="blue">9</div>
+        </section>
+      </div>
+
+      <div className="containerCara">
+        {/*//todo section color yellow */}
+        <section className="cara">
+          <div className="yellow">1</div>
+          <div className="yellow">2</div>
+          <div className="yellow">3</div>
+          <div className="yellow">4</div>
+          <div className="yellow">5</div>
+          <div className="yellow">6</div>
+          <div className="yellow">7</div>
+          <div className="yellow">8</div>
+          <div className="yellow">9</div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default CuboRubik;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CuboRubik from "./components/CuboRubik";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="container bg-slate-500 min-h-screen">
       hola soy el home
+      <CuboRubik/>
     </div>
   )
 }
