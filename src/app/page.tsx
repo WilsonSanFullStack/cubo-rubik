e312db297f1f8a765a619298b1c73041b1d7bdee
WilsonSanFullStack <clinicasystemlab@gmail.com>
2024-01-18 03:44:52 -0500

    component cubo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import CuboRubik from "./components/CuboRubik";
 
-
 export default function Home() {
   return (
-    <div className="container bg-slate-500 min-h-screen">
+    <div className="container bg-slate-500 min-h-screen text-center">
       hola soy el home
-      <CuboRubik/>
+      <div className="flex justify-center items-center">
+        <CuboRubik />
+      </div>
     </div>
-  )
+  );
 }
