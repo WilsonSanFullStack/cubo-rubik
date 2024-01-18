de8e25c96f524f620fc55d2ee5d51eb0a24c8899
WilsonSanFullStack <clinicasystemlab@gmail.com>
2024-01-18 06:04:46 -0500

    creando types y definiendo el cambio de styles

--- src/app/components/CuboRubik.tsx
+++ src/app/components/CuboRubik.tsx
@@ -1,89 +1,138 @@
 import React from "react";
-
+const clases = [
+  {
+    w1: 'white'
+  },
+  {
+    r1: 'red'
+  },
+  {
+    b1: 'blue'
+  },
+  {
+    o1: 'orange'
+  },
+  {
+    g1: 'green'
+  },
+  {
+    y1: 'yellow'
+  },
+  {
+    w2: 'white'
+  },
+  {
+    y2: 'yellow'
+  },
+]
 const CuboRubik = () => {
+
+  const white:White = {
+    a: 'w1',
+    b: 'w2',
+    c: 'w3',
+    d: 'w4',
+    e: 'w5',
+    f: 'w6',
+    g: 'w7',
+    h: 'w8',
+    i: 'w9'
+  }
+  const yellow:Yellow = {
+    a: 'w1',
+    b: 'w2',
+    c: 'w3',
+    d: 'w4',
+    e: 'w5',
+    f: 'w6',
+    g: 'w7',
+    h: 'w8',
+    i: 'w9'
+  }
+
   return (
     <div className="w-2/5 m-4  justify-center items-center">
       <div className="containerCara">
         {/*//todo section color green */}
         <section className="cara">
-          <div className="green">1</div>
-          <div className="green">2</div>
-          <div className="green">3</div>
-          <div className="green">4</div>
-          <div className="green">5</div>
-          <div className="green">6</div>
-          <div className="green">7</div>
-          <div className="green">8</div>
-          <div className="green">9</div>
+          <div className="green">G9</div>
+          <div className="green">G6</div>
+          <div className="green">G7</div>
+          <div className="green">G8</div>
+          <div className="green">G5</div>
+          <div className="green">G4</div>
+          <div className="green">G1</div>
+          <div className="green">G2</div>
+          <div className="green">G3</div>
         </section>
       </div>
 
       <div className="containerCara3">
         {/*//todo section color red */}
         <section className="cara">
-          <div className="red">1</div>
-          <div className="red">2</div>
-          <div className="red">3</div>
-          <div className="red">4</div>
-          <div className="red">5</div>
-          <div className="red">6</div>
-          <div className="red">7</div>
-          <div className="red">8</div>
-          <div className="red">9</div>
+          <div className="red">R9</div>
+          <div className="red">R8</div>
+          <div className="red">R1</div>
+          <div className="red">R2</div>
+          <div className="red">R5</div>
+          <div className="red">R4</div>
+          <div className="red">R3</div>
+          <div className="red">R6</div>
+          <div className="red">R7</div>
         </section>
         {/*//todo section color white */}
         <section className="cara">
-          <div className="white">1</div>
-          <div className="white">2</div>
-          <div className="white">3</div>
-          <div className="white">4</div>
-          <div className="white">5</div>
-          <div className="white">6</div>
-          <div className="white">7</div>
-          <div className="white">8</div>
-          <div className="white">9</div>
+          <div className="white">{white.a}</div>
+          <div className="white">{white.b}</div>
+          <div className="white">{white.c}</div>
+          <div className="white">{white.d}</div>
+          <div className="white">{white.e}</div>
+          <div className="white">{white.f}</div>
+          <div className="white">{white.g}</div>
+          <div className="white">{white.h}</div>
+          <div className="white">{white.i}</div>
         </section>
         {/*//todo section color orange */}
         <section className="cara">
-          <div className="orange">1</div>
-          <div className="orange">2</div>
-          <div className="orange">3</div>
-          <div className="orange">4</div>
-          <div className="orange">5</div>
-          <div className="orange">6</div>
-          <div className="orange">7</div>
-          <div className="orange">8</div>
-          <div className="orange">9</div>
+          <div className="orange">O3</div>
+          <div className="orange">O4</div>
+          <div className="orange">O7</div>
+          <div className="orange">O6</div>
+          <div className="orange">O5</div>
+          <div className="orange">O8</div>
+          <div className="orange">O9</div>
+          <div className="orange">O2</div>
+          <div className="orange">O1</div>
         </section>
       </div>
 
       <div className="containerCara">
         {/*//todo section color blue */}
         <section className="cara">
-          <div className="blue">1</div>
-          <div className="blue">2</div>
-          <div className="blue">3</div>
-          <div className="blue">4</div>
-          <div className="blue">5</div>
-          <div className="blue">6</div>
-          <div className="blue">7</div>
-          <div className="blue">8</div>
-          <div className="blue">9</div>
+          <div className="blue">B7</div>
+          <div className="blue">B8</div>
+          <div className="blue">B9</div>
+          <div className="blue">B6</div>
+          <div className="blue">B5</div>
+          <div className="blue">B2</div>
+          <div className="blue">B3</div>
+          <div className="blue">B4</div>
+          <div className="blue">B1</div>
         </section>
       </div>
 
       <div className="containerCara">
         {/*//todo section color yellow */}
         <section className="cara">
-          <div className="yellow">1</div>
-          <div className="yellow">2</div>
-          <div className="yellow">3</div>
-          <div className="yellow">4</div>
-          <div className="yellow">5</div>
-          <div className="yellow">6</div>
-          <div className="yellow">7</div>
-          <div className="yellow">8</div>
-          <div className="yellow">9</div>
+          <div className="yellow">Y9</div>
+          <div className="yellow">Y6</div>
+          <div className="yellow">Y7</div>
+          <div className="yellow">Y2</div>
+          <div className="yellow">Y5</div>
+          <div className="yellow">Y8</div>
+          <div className="yellow">Y3</div>
+          <div className="yellow">Y4</div>
+          <div className="yellow">Y1</div>
         </section>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CuboRubik from "./components/CuboRubik";
 export default function Home() {
   return (
     <div className="container bg-slate-500 min-h-screen text-center">
-      hola soy el home
+      
       <div className="flex justify-center items-center">
         <CuboRubik />
       </div>
